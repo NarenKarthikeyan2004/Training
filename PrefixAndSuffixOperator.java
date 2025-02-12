a03ca4cf85213b942f744830adbdaa67991cbb91
NarenKarthikeyan2004 <narenkarthik1710@gmail.com>
2025-02-12 14:32:04 +0530

    Add files via upload

---
+++ PrefixAndSuffixOperator.java
@@ -0,0 +1,7 @@
+class PrefixAndSuffixOperator{
+    public static void main(String[] args) {
+        int var1 = 5, var2 = 5;
+        System.out.println(var1++);
+        System.out.println(++var2);
+    }
+}
