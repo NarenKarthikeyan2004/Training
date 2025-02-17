60499b53f1ff1bf108d27ba8eab76cc42a6b64c6
NarenKarthikeyan2004 <narenkarthik1710@gmail.com>
2025-02-17 10:15:00 +0530

    Add files via upload

---
+++ Max2.java
@@ -0,0 +1,14 @@
+class Max2
+{
+	public static void main(String[] args) 
+	{
+	    int arr[]={1,2,3,4,5};
+	    int max=arr[0];
+	    for(int i=1; i<arr.length -2; i++)
+	    {
+	        if(arr[i]>max)
+	        max=arr[i];
+	    }
+	    System.out.println("Maximum value: " + max);
+	}
+}
