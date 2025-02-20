d45793b7cf3a8cfe84f2f1f22a74d83341cbd668
NarenKarthikeyan2004 <narenkarthik1710@gmail.com>
2025-02-20 16:18:06 +0530

    Car1.java

--- Car1.java
+++ Car1.java
@@ -1,17 +1,17 @@
 class Car1{
     String model;
-    Car() {
+    Car1() {
         System.out.println("DC");
     }
-    Car(String model)  {
+    Car1(String model)  {
         this.model = model;
     }
     void display() {
         System.out.println("Model: " + model);
     }
     public static void main(String[] args) {
-        Car c = new Car("AUDI");
-        Car d = new Car();
+        Car1 c = new Car1("Toyata");
+        Car1 d = new Car1();
         c.display();
     }
 }
